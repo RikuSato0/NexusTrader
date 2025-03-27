b04c48385dd29c1cb992a39947a4e91e3255032c
RikuSato0 <blockify41@gmail.com>
2025-03-27 04:39:38 +0000

    Enhance Order Management System with order status handling and timeout feature
    - Added `order_submit_timeout` parameter to `OrderManagementSystem` for managing order submission timeouts.
    - Implemented `_order_status_update` method to handle various order statuses and log updates accordingly.
    - Introduced `_handle_waiting_order_event` method to process waiting orders and update their status based on registration.
    - Refactored order handling logic in `_handle_order_event` to utilize the new waiting order queue for improved order management.

--- nexustrader/base/oms.py
+++ nexustrader/base/oms.py
@@ -18,6 +18,7 @@ class OrderManagementSystem(ABC):
         msgbus: MessageBus,
         task_manager: TaskManager,
         registry: OrderRegistry,
+        order_submit_timeout: float | None = None,
     ):
         self._log = SpdLog.get_logger(
             name=type(self).__name__, level="DEBUG", flush=True
@@ -26,14 +27,58 @@ class OrderManagementSystem(ABC):
         self._msgbus = msgbus
         self._task_manager = task_manager
         self._registry = registry
-
+        self._order_submit_timeout = order_submit_timeout
         self._order_msg_queue: asyncio.Queue[Order] = asyncio.Queue()
+        self._waiting_order_msg_queue: asyncio.Queue[Order] = asyncio.Queue()
 
     def _add_order_msg(self, order: Order):
         """
         Add an order to the order message queue
         """
         self._order_msg_queue.put_nowait(order)
+        
+    
+    def _order_status_update(self, order: Order):
+        match order.status:
+            case OrderStatus.ACCEPTED:
+                self._log.debug(f"ORDER STATUS ACCEPTED: {str(order)}")
+                self._cache._order_status_update(order)
+                self._msgbus.send(endpoint="accepted", msg=order)
+            case OrderStatus.PARTIALLY_FILLED:
+                self._log.debug(f"ORDER STATUS PARTIALLY FILLED: {str(order)}")
+                self._cache._order_status_update(order)
+                self._msgbus.send(endpoint="partially_filled", msg=order)
+            case OrderStatus.CANCELED:
+                self._log.debug(f"ORDER STATUS CANCELED: {str(order)}")
+                self._cache._order_status_update(order)
+                self._msgbus.send(endpoint="canceled", msg=order)
+                # self._registry.remove_order(order) #NOTE: order remove should be handle separately
+            case OrderStatus.FILLED:
+                self._log.debug(f"ORDER STATUS FILLED: {str(order)}")
+                self._cache._order_status_update(order)
+                self._msgbus.send(endpoint="filled", msg=order)
+                # self._registry.remove_order(order) #NOTE: order remove should be handle separately
+            case OrderStatus.EXPIRED:
+                self._log.debug(f"ORDER STATUS EXPIRED: {str(order)}")
+                self._cache._order_status_update(order)
+            case _:
+                self._log.error(f"ORDER STATUS UNKNOWN: {str(order)}")
+    
+    async def _handle_waiting_order_event(self):
+        """
+        Handle the waiting order message
+        """
+        while True:
+            try:
+                order = await self._waiting_order_msg_queue.get()
+                await self._registry.wait_for_order_id(order.id, self._order_submit_timeout)
+                uuid = self._registry.get_uuid(order.id)
+                order.uuid = uuid
+                self._order_status_update(order)
+                self._waiting_order_msg_queue.task_done()
+            except Exception as e:
+                self._log.error(f"Error in handle_waiting_order_event: {e}")
+    
 
     async def _handle_order_event(self):
         """
@@ -47,34 +92,11 @@ class OrderManagementSystem(ABC):
                 uuid = self._registry.get_uuid(order.id)
                 if not uuid:
                     self._log.debug(f"WAIT FOR ORDER ID: {order.id} TO BE REGISTERED")
-                    await self._registry.wait_for_order_id(order.id) #NOTE: need to wait for the order id to be registered
-                    uuid = self._registry.get_uuid(order.id)
+                    await self._waiting_order_msg_queue.put(order)
+                    # await self._registry.wait_for_order_id(order.id) #NOTE: need to wait for the order id to be registered
+                    # uuid = self._registry.get_uuid(order.id)
                 order.uuid = uuid
-
-                match order.status:
-                    case OrderStatus.ACCEPTED:
-                        self._log.debug(f"ORDER STATUS ACCEPTED: {str(order)}")
-                        self._cache._order_status_update(order)
-                        self._msgbus.send(endpoint="accepted", msg=order)
-                    case OrderStatus.PARTIALLY_FILLED:
-                        self._log.debug(f"ORDER STATUS PARTIALLY FILLED: {str(order)}")
-                        self._cache._order_status_update(order)
-                        self._msgbus.send(endpoint="partially_filled", msg=order)
-                    case OrderStatus.CANCELED:
-                        self._log.debug(f"ORDER STATUS CANCELED: {str(order)}")
-                        self._cache._order_status_update(order)
-                        self._msgbus.send(endpoint="canceled", msg=order)
-                        # self._registry.remove_order(order) #NOTE: order remove should be handle separately
-                    case OrderStatus.FILLED:
-                        self._log.debug(f"ORDER STATUS FILLED: {str(order)}")
-                        self._cache._order_status_update(order)
-                        self._msgbus.send(endpoint="filled", msg=order)
-                        # self._registry.remove_order(order) #NOTE: order remove should be handle separately
-                    case OrderStatus.EXPIRED:
-                        self._log.debug(f"ORDER STATUS EXPIRED: {str(order)}")
-                        self._cache._order_status_update(order)
-                    case _:
-                        self._log.error(f"ORDER STATUS UNKNOWN: {str(order)}")
+                self._order_status_update(order)
                 self._order_msg_queue.task_done()
             except Exception as e:
                 self._log.error(f"Error in handle_order_event: {e}")
@@ -86,4 +108,5 @@ class OrderManagementSystem(ABC):
         self._log.debug("OrderManagementSystem started")
 
         # Start order and position event handlers
+        self._task_manager.create_task(self._handle_waiting_order_event())
         self._task_manager.create_task(self._handle_order_event())
--- nexustrader/core/registry.py
+++ nexustrader/core/registry.py
@@ -1,8 +1,8 @@
 import asyncio
-from collections import defaultdict
 from typing import Optional
 from nexustrader.core.log import SpdLog
 from nexustrader.schema import Order
+from typing import Dict
 
 class OrderRegistry:
     def __init__(self):
@@ -11,13 +11,14 @@ class OrderRegistry:
         )
         self._uuid_to_order_id = {}
         self._order_id_to_uuid = {}
-        self._uuid_init_events = defaultdict(asyncio.Event)
+        self._futures: Dict[str, asyncio.Future] = {}
 
     def register_order(self, order: Order) -> None:
         """Register a new order ID to UUID mapping"""
         self._order_id_to_uuid[order.id] = order.uuid
         self._uuid_to_order_id[order.uuid] = order.id
-        self._uuid_init_events[order.id].set() # the order id is linked to the order submit uuid
+        if order.id in self._futures and not self._futures[order.id].done():
+            self._futures[order.id].set_result(None) # release the waiting task
         self._log.debug(f"[ORDER REGISTER]: linked order id {order.id} with uuid {order.uuid}")
 
     def get_order_id(self, uuid: str) -> Optional[str]:
@@ -28,12 +29,19 @@ class OrderRegistry:
         """Get UUID by order ID"""
         return self._order_id_to_uuid.get(order_id, None)
 
-    async def wait_for_order_id(self, order_id: str) -> None:
+    async def wait_for_order_id(self, order_id: str, timeout: float | None = None) -> None:
         """Wait for an order ID to be registered"""
-        await self._uuid_init_events[order_id].wait()
+        # await self._uuid_init_events[order_id].wait()
+        self._futures[order_id] = asyncio.Future()
+        if timeout:
+            await asyncio.wait_for(self._futures[order_id], timeout)
+            self._log.warn(f"order id {order_id} registered timeout")
+        else:
+            await self._futures[order_id]
+        self._futures.pop(order_id)
 
     def remove_order(self, order: Order) -> None:
         """Remove order mapping when no longer needed"""
         self._order_id_to_uuid.pop(order.id, None)
         self._uuid_to_order_id.pop(order.uuid, None)
-        self._uuid_init_events.pop(order.id, None)
+        # self._uuid_init_events.pop(order.id, None)
